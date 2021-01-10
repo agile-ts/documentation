764b45a69491115b774baa8598479a15f3d90390
Benno Kohrs <bennokohrs@gmail.com>
2021-01-10 08:37:02 +0100

    removed embedded code sandbox

--- src/pages/LandingPage/index.tsx
+++ src/pages/LandingPage/index.tsx
@@ -7,17 +7,6 @@ import styled from "styled-components";
 import {useWindowSize} from "../../hooks/useWindowSize";
 import Layout from '@theme/Layout';
 
-const makeExample = (uri) => `<iframe
-  src="https://codesandbox.io/embed/${uri}?fontsize=14&hidenavigation=0&theme=light&view=preview"
-  style="width:100%; height:500px; border:0; border-radius: 4px; overflow:hidden;"
-  title="Chat-Example"
-  allow="geolocation; microphone; camera; midi; vr; accelerometer; gyroscope; payment; ambient-light-sensor; encrypted-media; usb"
-  sandbox="allow-modals allow-forms allow-popups allow-scripts allow-same-origin"
-  ></iframe>`;
-
-
-const firstStateExample = makeExample('agilets-first-state-f12cz');
-
 const LandingPage: React.FC = () => {
     const windowSize = useWindowSize();
     return (
@@ -40,7 +29,6 @@ const LandingPage: React.FC = () => {
                     </CodeExampleContainer>
                 </div>
             }
-            <div id="firstStateExample" dangerouslySetInnerHTML={{ __html: firstStateExample }} />
         </Layout>
     )
 }
